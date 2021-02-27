50e07ae573172d484a2246fef33b7b69451f529f
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-02-27 11:59:39 -0800

    update

--- src/components/WelcomeBackground.js
+++ src/components/WelcomeBackground.js
@@ -1,14 +1,20 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
 import styled from "styled-components"
 
-const BackgroundWrapper = styled.div`
-  height: 70vh;
-  width: 100vw;
-`
+import BackgroundImage from "gatsby-background-image"
 
-const StyledImg = styled(Img)``
+const StyledBackgroundImage = styled(BackgroundImage)`
+  width: 100vw;
+  height: 80vh;
+  object-fit: cover;
+  opacity: 1 !important;
+  background: linear-gradient(
+    to right bottom,
+    rgba(255, 255, 255, 0.2) 10%,
+    rgba(0, 0, 0, 0.9) 90%
+  );
+`
 
 const WelcomeBackground = () => {
   const data = useStaticQuery(graphql`
@@ -22,14 +28,13 @@ const WelcomeBackground = () => {
       }
     }
   `)
+
+  const imageData = data.file.childImageSharp.fluid
+
   return (
-    <BackgroundWrapper>
-      <StyledImg
-        style={{ height: "100%", width: "100%" }}
-        imgStyle={{ objectFit: "conatin" }}
-        fluid={data.file.childImageSharp.fluid}
-      />
-    </BackgroundWrapper>
+    <StyledBackgroundImage fluid={imageData}>
+      <h2>Background Image</h2>
+    </StyledBackgroundImage>
   )
 }
 
