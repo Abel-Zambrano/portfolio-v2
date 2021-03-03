53c30d838ec2caec5327a0aa3ea8efa00b5219b3
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-03-02 20:00:17 -0800

    update

---
+++ src/JS/skills.js
@@ -0,0 +1,15 @@
+const skillsData = {
+  details: "",
+  logos: [
+    "html",
+    "css",
+    "javascript",
+    "react",
+    "gatsby",
+    "graphql",
+    "node.js",
+    "mongodb",
+    "python",
+    "mysql",
+  ],
+}
