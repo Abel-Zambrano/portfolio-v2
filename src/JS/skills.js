69dd5d7a0c2f58508e79a5aa922b1eba9e90f527
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-03-07 10:41:05 -0800

    added data to skills.js

--- src/JS/skills.js
+++ src/JS/skills.js
@@ -7,6 +7,66 @@ const skillsData = {
       name: "html",
       logo: "SiHtml5",
     },
+    {
+      id: 2,
+      name: "css",
+      logo: "SiCss3",
+    },
+    {
+      id: 3,
+      name: "sass",
+      logo: "SiSass",
+    },
+    {
+      id: 4,
+      name: "styled-components",
+      logo: "SiStyledComponents",
+    },
+    {
+      id: 5,
+      name: "javascript",
+      logo: "SiJavascript",
+    },
+    {
+      id: 6,
+      name: "react",
+      logo: "SiReact",
+    },
+    {
+      id: 7,
+      name: "gatsby",
+      logo: "SiGatsby",
+    },
+    {
+      id: 8,
+      name: "graphql",
+      logo: "SiGraphql",
+    },
+    {
+      id: 9,
+      name: "github",
+      logo: "SiGithub",
+    },
+    {
+      id: 9,
+      name: "node.js",
+      logo: "SiNodeDotJs",
+    },
+    {
+      id: 10,
+      name: "mongodb",
+      logo: "SiMongodb",
+    },
+    {
+      id: 11,
+      name: "python",
+      logo: "SiPython",
+    },
+    {
+      id: 12,
+      name: "mysql",
+      logo: "SiMysql",
+    },
   ],
 }
 
