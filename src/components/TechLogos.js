b3b8edaf89ddffc57e6081b6ea882cc75b7e327e
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-03-08 22:23:17 -0800

    update

--- src/components/TechLogos.js
+++ src/components/TechLogos.js
@@ -1,23 +1,13 @@
 import React from "react"
 import styled from "styled-components"
-import { SiMercedes } from "@react-icons/all-files/si/SiMercedes"
 import skillsData from "../JS/skills"
 
 const StyledTechLogos = styled.ul`
   list-style: none;
-  text-transform: uppercase;
-  color: var(--color-pink);
 `
 
 const TechLogos = () => {
-  return (
-    <StyledTechLogos>
-      <SiMercedes />
-      {skillsData.map(logo => {
-        return <li key={logo.id}>{logo.name}</li>
-      })}
-    </StyledTechLogos>
-  )
+  return <StyledTechLogos></StyledTechLogos>
 }
 
 export default TechLogos
