4881b76412c635011ae7c6affd333a0e2dd44670
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-03-09 19:30:21 -0800

    update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,21 +7,28 @@ import data from "../JS/data"
 
 const SkillsWrapper = styled.article`
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: space-evenly;
   align-items: center;
   text-align: center;
   height: 100%;
   width: 100%;
 `
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+`
+
 const Skills = () => {
   return (
-    <>
+    <SkillsWrapper>
       <CardTitle>{data[1].title}</CardTitle>
-      <SkillsWrapper>
+      <Content>
         <TechText />
         <TechLogos />
-      </SkillsWrapper>
-    </>
+      </Content>
+    </SkillsWrapper>
   )
 }
 
--- src/components/TechText.js
+++ src/components/TechText.js
@@ -2,14 +2,39 @@ import React from "react"
 import styled from "styled-components"
 import skillsData from "../JS/skills"
 
+const frontEndSkills = skillsData.slice(0, 9)
+
+const fName = frontEndSkills.map(skill => {
+  return skill.name
+})
+
+console.log(frontEndSkills[0].name)
+
+console.log(frontEndSkills)
+
 const TechTextWrapper = styled.div``
 
-const Text = styled.p``
+const Text = styled.p`
+  max-width: 58rem;
+  line-height: 2rem;
+  font-size: 1.52rem;
+  color: var(--color-jetblack);
+  margin: 1rem auto;
+`
+const Span = styled.span`
+  font-family: "Roboto Mono", monospace;
+  color: var(--color-primary-dark);
+`
 
 const TechText = () => {
   return (
     <TechTextWrapper>
-      <Text>Front-End Skills: Back-End Skills:</Text>
+      <Text>
+        Front-End Skills: <Span>{fName}</Span>
+      </Text>
+      <Text>
+        Back-End Skills: <Span></Span>
+      </Text>
     </TechTextWrapper>
   )
 }
