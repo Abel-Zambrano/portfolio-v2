45fff32d69b1a42bfb40892a419a3ef128079771
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-04-05 18:28:46 -0700

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,9 +35,9 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
-        contentTypes: [`projects`, `user`],
+        contentTypes: [`projects`],
         //If using single types place them in this array.
-        singleTypes: [`home-page`, `contact`],
+        singleTypes: [],
         // Possibility to login with a strapi user, when content types are not publically available (optional).
         loginData: {
           identifier: "",
