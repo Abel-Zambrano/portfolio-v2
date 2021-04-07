0b87ed5e656507b6098a16ac6e162675e3f085a3
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-04-06 20:02:48 -0700

    update

--- src/components/sections/projects/Test.js
+++ src/components/sections/projects/Test.js
@@ -2,49 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 const Test = () => {
-  const data = useStaticQuery(graphql`
-    {
-      allStrapiProjects {
-        edges {
-          node {
-            gitURL
-            id
-            title
-            webURL
-            preview {
-              url
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  const query = data.allStrapiProjects.edges
-  const imgQuery = query[0].node.preview[0].url
-  console.log(imgQuery)
-  console.log(query)
-  return (
-    <div>
-      {query.map(e => {
-        return (
-          <div>
-            <h1>{e.node.title}</h1>
-            <a href={e.node.webURL}>Web Site</a>
-            <p>{e.node.preview.url}</p>
-            {/* <img
-              src={`http://localhost:1337${e.node.preview[0].url}`}
-              alt={e.node.id}
-            /> */}
-          </div>
-        )
-      })}
-
-      {/* <p>{query.title}</p>
-      <img src={query.webURL} alt="rocket burger" />
-      <a href={query.gitURL}>GITHUB</a> */}
-    </div>
-  )
+  return <div></div>
 }
 
 export default Test
